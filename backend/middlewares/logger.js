a1736104720ce09a16e7180948486ad72ab2201d
Alexandr Balkov <Harleta1907@yandex.ru>
2021-10-06 16:27:59 +0200

    fixed logger

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -1,11 +1,11 @@
-const winston = require('express')
-const expressWinston = require('express')
+const winston = require('winston')
+const expressWinston = require('express-winston')
 
 const requestLogger = expressWinston.logger({
   transports: [
     new winston.transports.File({ filename: 'request.log' }),
   ],
-  format: winston.forma.json(),
+  format: winston.format.json(),
 })
 
 const errorLogger = expressWinston.errorLogger({
